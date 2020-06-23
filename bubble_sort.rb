2ed654ca6dfceb030d4cf2201c3ceb64141fa837
Felipe Dacal Fragoso <fefragoso@gmail.com>
2020-06-23 14:36:23 +0200

    Update bubble_sort.rb

---
+++ bubble_sort.rb
@@ -0,0 +1,18 @@
+def bubble_sort_by(arr)
+  a = -1
+   while a != 0 do
+    a = 0
+    for i in 0..((arr.length) -2)
+      if arr[i] > arr[i + 1]
+        temp = arr[i]
+        arr[i] = arr[i + 1]
+        arr[i + 1] = temp
+        a += 1
+      end
+    end
+  end
+  return arr
+end
+  
+p bubble_sort_by([2, 4, 1, 8, 6, 21, 17, 3]) 
+  
