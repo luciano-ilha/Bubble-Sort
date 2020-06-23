19992b27b9dafc6f23c35c5203e468006fbef7cd
Luciano Ilha Carbonell <ilha.dev.br@gmail.com>
2020-06-23 10:12:30 -0300

    Fix linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,8 +1,8 @@
 def bubble_sort_by(arr)
   a = -1
-   while a != 0 do
+  while a != 0
     a = 0
-    for i in 0..((arr.length) -2)
+    for i in 0..(arr.length) - 2
       if arr[i] > arr[i + 1]
         temp = arr[i]
         arr[i] = arr[i + 1]
@@ -13,6 +13,5 @@ def bubble_sort_by(arr)
   end
   return arr
 end
-  
-p bubble_sort_by([2, 4, 1, 8, 6, 21, 17, 3]) 
-  
+
+p bubble_sort_by([2, 4, 1, 8, 6, 21, 17, 3])
